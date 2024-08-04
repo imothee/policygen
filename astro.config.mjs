fc18b748001bf4d12b1a943df10209cc483e631e
Tim Marks <t@imothee.com>
2024-08-04 15:10:28 -0700

    feat: adds sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), sitemap(), robotsTxt()],
   site: "https://policygen.xyz"
 });
